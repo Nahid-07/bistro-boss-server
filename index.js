ed629ea00190f8c30e6fcb41b1f190e1e638619b
Nahid-07 <nahidasif1998@gmail.com>
2024-01-29 20:36:21 +0600

    user admin get api created

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ require("dotenv").config();
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const uri = `mongodb+srv://${process.env.BISTRO_DB}:${process.env.BISTRO_DB_PASS}@cluster0.ugpmzsn.mongodb.net/?retryWrites=true&w=majority`;
 const port = process.env.PORT || 5000;
-const jwt = require('jsonwebtoken')
+const jwt = require("jsonwebtoken");
 
 // middlewares
 app.use(cors());
@@ -13,22 +13,6 @@ app.use(express.json());
 app.get("/", (req, res) => {
   res.send("boss server is running");
 });
-
-// JWT verify 
-const jwtVerify = (req,res,next) =>{
-  const authorization = req.headers.authorization;
-  if(!authorization){
-    return res.status(401).send({error : true, message: "Unauthorized access"})
-  };
-  const token = authorization.split(" ")[1];
-  jwt.verify(token, process.env.ACCESS_TOKEN, (err, decode)=>{
-    if(err){
-      return res.status(401).send({error : true, message: "Unauthorized access"})
-    }
-    req.decode = decode;
-    next()
-  })
-}
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -67,67 +51,66 @@ async function run() {
       res.send(result);
     });
     // get cart item
-    app.get("/cart", jwtVerify, async (req, res) => {
-     const email = req.query.email;
-     const decodeEmail = req.decode.email;
-     if(email !== decodeEmail){
-        return res.status(401).send({error : true, message: "Unauthorized access"})
-     }
-     if(!email){
-      res.send([])
-     }
-     const query = {userEmail : email}
+    app.get("/cart", async (req, res) => {
+      const email = req.query.email;
+      if (!email) {
+        res.send([]);
+      }
+
+      const query = { userEmail: email };
       const cartData = await cartCollection.find(query).toArray();
       res.send(cartData);
     });
-    
+
     // delete order from the user cart
-    app.delete('/cart/:id', async(req,res)=>{
+    app.delete("/cart/:id", async (req, res) => {
       const id = req.params.id;
-      const query = {_id : new ObjectId(id) };
+      const query = { _id: new ObjectId(id) };
       const result = await cartCollection.deleteOne(query);
-      res.send(result)
+      res.send(result);
     });
 
     // save user to the database
-    app.post('/user', async(req,res)=>{
-       const userData = req.body;
-       const result = await userCollection.insertOne(userData);
-       res.send(result)
-    })
+    app.post("/user", async (req, res) => {
+      const userData = req.body;
+      const result = await userCollection.insertOne(userData);
+      res.send(result);
+    });
     // users get api
-    app.get('/users', async(req,res)=>{
+    app.get("/users", async (req, res) => {
       const query = {};
       const result = await userCollection.find(query).toArray();
-      res.send(result)  
+      res.send(result);
     });
 
-    // create admin api 
-    app.patch('/users/admin/:id', async(req,res)=>{
+    // create admin api
+    app.patch("/users/admin/:id", async (req, res) => {
       const id = req.params.id;
-      const filter = {_id : new ObjectId(id)};
+      const filter = { _id: new ObjectId(id) };
       const updateDoc = {
         $set: {
-          role: 'admin'
+          role: "admin",
         },
       };
       const result = await userCollection.updateOne(filter, updateDoc);
-      res.send(result)
+      res.send(result);
     });
 
     // user dlt api
-    app.delete('/users/delete/:id', async(req,res)=>{
+    app.delete("/users/delete/:id", async (req, res) => {
       const id = req.params.id;
-      const query = {_id : new ObjectId(id)};
+      const query = { _id: new ObjectId(id) };
       const result = await userCollection.deleteOne(query);
-      res.send(result)
+      res.send(result);
     });
-    // JWT ASSIGN
-    app.post('/jwt',(req,res)=>{
-        const user = req.body;
-        const token = jwt.sign({user},process.env.ACCESS_TOKEN,{ expiresIn: '1h' } )
-        res.send(token)
-    })
+    // admin checking route
+    app.get("/users/admin/:email", async (req, res) => {
+      const email = req.params.email;
+      const query = { userEmail: email };
+      const result = await userCollection.findOne(query);
+      const admin = { admin: result?.role === "admin" };
+      res.send(admin);
+    });
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
